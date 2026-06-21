81e20dbe0a19178b1ab3ce386ff6435bc4544fbc
bengobro <starodubtsev.ma03@gmail.com>
2026-06-21 15:07:54 +0300

    Initial commit

---
+++ src/main/java/ru/bengobro/electronic_shop/ElectronicShopApplication.java
@@ -0,0 +1,13 @@
+package ru.bengobro.electronic_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bengobro/electronic_shop/ElectronicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bengobro.electronic_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
